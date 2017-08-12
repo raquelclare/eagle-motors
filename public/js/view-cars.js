07755fda80ec5c54b09247fbf255547837eb7282
Raquel Rodriguez <raquelclare@Raquels-MacBook-Air.local>
2017-08-12 07:59:34 -0500

    we are not needing some of the js files

--- public/js/view-cars.js
+++ public/js/view-cars.js
@@ -1,34 +1,34 @@
 console.log("hello");
 // Need to dynamically create divs for cars already stored into the database
 // carContainer holds all of our cars
-    var carContainer = $(".car-col");
-// This function grabs cars from the database and updates the view
-    // Useful if someone wants to search for a specific make
-    function getCars() {
-        var carMakeString = make || "";
-        if (carMakeString) {
-        carMakeString = "/make/" + carMakeString;
-        }
-        $.get("/" + carMakeString, function(data) {
-        console.log("Cars", data);
-        cars = data;
-        if (!cars || !cars.length) {
-            displayEmpty();
-        }
-        else {
-            initializeRows();
-        }
-        });
-    }
-    // Getting the initial list of cars
-    getCars();
-    // InitializeRows handles appending all of our constructed post HTML inside
-    // carContainer
-    function initializeRows() {
-        carContainer.empty();
-        var postsToAdd = [];
-        for (var i = 0; i < cars.length; i++) {
-        carsToAdd.push(createNewRow(cars[i]));
-        }
-        carContainer.append(carsToAdd);
+//     var carContainer = $(".car-col");
+// // This function grabs cars from the database and updates the view
+//     // Useful if someone wants to search for a specific make
+//     function getCars() {
+//         var carMakeString = make || "";
+//         if (carMakeString) {
+//         carMakeString = "/make/" + carMakeString;
+//         }
+//         $.get("/" + carMakeString, function(data) {
+//         console.log("Cars", data);
+//         cars = data;
+//         if (!cars || !cars.length) {
+//             displayEmpty();
+//         }
+//         else {
+//             initializeRows();
+//         }
+//         });
+//     }
+//     // Getting the initial list of cars
+//     getCars();
+//     // InitializeRows handles appending all of our constructed post HTML inside
+//     // carContainer
+//     function initializeRows() {
+//         carContainer.empty();
+//         var postsToAdd = [];
+//         for (var i = 0; i < cars.length; i++) {
+//         carsToAdd.push(createNewRow(cars[i]));
+//         }
+//         carContainer.append(carsToAdd);
     }
